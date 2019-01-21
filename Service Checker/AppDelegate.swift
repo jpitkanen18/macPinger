dc64f585af22551b00500dd6bd21d53c4842065b
benisbenis <jesemexicoman@gmail.com>
2019-01-21 10:40:23 +0200

    Initial Commit

---
+++ Service Checker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Service Checker
+//
+//  Created by Jese on 21/01/2019.
+//  Copyright © 2019 jbenisoy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Service Checker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Service Checker
+//
+//  Created by Jese on 21/01/2019.
+//  Copyright © 2019 jbenisoy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
